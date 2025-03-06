58d4542f64be1f4b2402640013599022eb705ea9
Thatonerookiedev <shatoriag01@gmail.com>
2025-03-06 13:25:11 -0600

    Dynamic monkeys created

--- Amplify/Services/MonkeyService.cs
+++ Amplify/Services/MonkeyService.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MonkeyFinderHybrid.Services;
+using MonkeyFinderHybrid.Model;
+using System.Net.Http.Json;
 
 public class MonkeyService
 {
+    private List<Monkey> monkeysList = new();
+    private readonly HttpClient httpClient;
 
+    public MonkeyService() {
+        httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeys() {
+        //small cacheing
+        if (monkeysList.Count > 0) {
+            return monkeysList;
+        }
+
+        var response = await httpClient.GetAsync("https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json");
+
+        if (response.IsSuccessStatusCode) {
+            var monkeysResult = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+
+            if (monkeysResult is not null) {
+                monkeysList = monkeysResult;
+            }
+        }
+
+        return monkeysList;
+    }
 }
